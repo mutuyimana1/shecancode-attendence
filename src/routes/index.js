83d023e2a371e98c342e1f0b06a87a8272fd05d5
UWAJENEZANadine <adelineu640@gmail.com>
2022-10-03 09:56:38 +0200

    frontend attendance project initialization

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Routes, Route} from "react-router-dom"
+import Home from "../views/home";
+
+
+const Index = () =>{
+  return(
+    <Routes>
+      <Route path= "/" element = {<Home/>} />
+      <Route path= "home" element = {<Home/>} />
+
+    </Routes>
+  )
+}
+export default Index;
