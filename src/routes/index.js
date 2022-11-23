8dc63b31471f03af5bbc602f581d3e9849a8fcc1
ukuli piete <pieteukuri@gmail.com>
2022-11-23 10:28:04 +0200

    frontend dashboard commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import Home from "../views/home";
 // import DashLayout from "../components/Dashboard/Layout"
-import View from "../views/Dashboard/DashLayout"
+import View from "../views/Dashboard/DashLayout";
+// import AddStudent from "../components/AddStudent/AddStudent";
 import Signin from "../components/Forms/Signin";
-
+import AdminDashboard from "../views/Dashboard/AdminDashboard";
 
 
 
@@ -17,6 +18,8 @@ const Index = () =>{
       <Route path= "home" element = {<Home/>} />
       <Route path= "/Dashboard" element = {<View/>} />
       <Route path= "/signin" element = {<Signin/>} />
+      {/* <Route path="/addstudent" element={<AddStudent/>}/> */}
+      <Route path="/admindashboard" element={<AdminDashboard/>}/>
 
     </Routes>
     
---
+++ src/views/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,88 @@
+// import React from 'react'
+import React from 'react-dom'
+import '../Dashboard/admin.css'
+import {Link } from 'react-router-dom'
+
+const AdminDashboard = () => {
+  
+  return(
+    
+<div >
+    <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-700 text-black dark:text-white">
+      <div className="fixed w-full flex items-center justify-between h-14 text-white z-10">
+        <div className="flex items-center justify-start md:justify-center pl-3 w-14 md:w-64 h-14 bg-gray-800 dark:bg-gray-800 border-none">
+          <img className="w-7 h-7 md:w-10 md:h-10 mr-2 rounded-md overflow-hidden" src="https://therminic2018.eu/wp-content/uploads/2018/07/dummy-avatar.jpg" />
+          <span className="hidden md:block">ADMIN</span>
+        </div>
+        <div className="flex justify-between items-center h-14 bg-gray-800 dark:bg-gray-800 header-right">
+          
+
+        </div>
+      </div>
+      <div className="fixed flex flex-col top-14 left-0 w-14 hover:w-64 md:w-64 bg-gray-900 dark:bg-gray-900 h-full text-white transition-all duration-300 border-none z-10 sidebar">
+        <div className="overflow-y-auto overflow-x-hidden flex flex-col justify-between flex-grow">
+          <ul className="flex flex-col py-4 space-y-1 ml-0 pl-0">
+            
+            <li>
+              <Link to="/admindashboard" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
+                <span className="inline-flex justify-center items-center ml-4">
+                  <svg className="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+                </span>
+                <span className="ml-2 text-2xl tracking-wide truncate">Dashboard</span>
+              </Link>
+            </li>
+          </ul>
+          <p className="mb-14 px-5 py-3 hidden md:block text-center text-xl">Copyright @2022</p>
+        </div>
+      </div>
+    
+      <div className="h-full ml-14 mt-14 mb-10 md:ml-64">
+    
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4 gap-4">
+          <div className="bg-blue-300 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
+            <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
+              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+            </div>
+            <div className="text-right">
+              <p className="text-2xl">8</p>
+              <p>Mentors</p>
+            </div>
+          </div>
+          <div className="bg-blue-300 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
+            <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
+              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
+            </div>
+            <div className="text-right">
+              <p className="text-2xl">557</p>
+              <p>Classrooms</p>
+            </div>
+          </div>
+          <div className="bg-blue-300 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
+            <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
+              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
+            </div>
+            <div className="text-right">
+              <p className="text-2xl">57</p>
+              <p>Courses</p>
+            </div>
+          </div>
+          <div className="bg-blue-300 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
+            <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
+            <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+            </div>
+            <div className="text-right">
+              <p className="text-2xl">total</p>
+              <p>Students</p>
+            </div>
+          </div>
+        </div>
+    
+
+
+
+      </div>
+    </div>
+  </div>)
+}
+
+export default AdminDashboard
